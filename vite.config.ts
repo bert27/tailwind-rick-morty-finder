4077e264427678a54941a328b81c550287ea5f3f
bert27 <zzopp@hotmail.com>
2025-01-26 14:05:40 +0100

    fix: configure absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      services: path.resolve(__dirname, './src/services'),
+      context: path.resolve(__dirname, './src/context'),
+      components: path.resolve(__dirname, './src/components'),
+      models: path.resolve(__dirname, './src/models'),
+    },
+  },
+});
